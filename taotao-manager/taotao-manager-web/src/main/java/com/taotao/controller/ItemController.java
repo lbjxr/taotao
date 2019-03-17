cd36654016cf8ae12cc4f549da0bbe6d1a39da0c
LBJ <1172907815@qq.com>
2019-03-17 16:08:01 +0800

    商品添加功能的实现，不包括商品描述的保存

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -3,12 +3,10 @@ package com.taotao.controller;
 import com.taotao.pojo.EUDataGridResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
+import com.taotao.utils.TaotaoResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,5 +56,17 @@ public class ItemController {
         System.out.print("\n=========方法被执行=============\n");
         System.out.print("item=" + item.get(0).getTitle() + "\n");
         return item;
+    }
+
+    /**
+     * 商品添加方法
+     * @param item
+     * @return
+     */
+    @PostMapping(value = "/item/save")
+    @ResponseBody
+    public TaotaoResult createItem(TbItem item){
+        TaotaoResult taotaoResult = itemService.createItem(item);
+        return taotaoResult;
     }
 }
