2170dc877c42cb20505fa62de232e6b16ee29bf7
LBJ <1172907815@qq.com>
2019-03-18 21:39:15 +0800

    保存商品参数

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemParamService.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemParamService.java
@@ -1,7 +1,11 @@
 package com.taotao.service;
 
 import com.taotao.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItemParam;
+import com.taotao.utils.TaotaoResult;
 
 public interface ItemParamService {
     EUDataGridResult getItemParamList(int page, int rows);
+    TaotaoResult getItemParamByCid(long cid);
+    TaotaoResult insertItemPatam(TbItemParam itemParam);
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemParamController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemParamController.java
@@ -1,26 +1,55 @@
 package com.taotao.controller;
 
 import com.taotao.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItemParam;
 import com.taotao.service.ItemParamService;
+import com.taotao.utils.TaotaoResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 商品参数控制层
  */
 @Controller
+@RequestMapping("/item/param")
 public class ItemParamController {
     @Autowired
     ItemParamService itemParamService;
 
-    @RequestMapping(value = "/item/param/list")
+    /**
+     * 商品参数列表显示
+     * @param page
+     * @param rows
+     * @return
+     */
+    @GetMapping(value = "/list")
     @ResponseBody
     public EUDataGridResult getItemParamList(Integer page, Integer rows){
         EUDataGridResult result = itemParamService.getItemParamList(page, rows);
         return  result;
     }
 
+    /**
+     * 根据cid查询商品参数
+     * @param itemCatId
+     * @return
+     */
+    @GetMapping("/query/itemcatid/{itemCatId}")
+    @ResponseBody
+    public TaotaoResult getItemParamByCid(@PathVariable long itemCatId){
+        TaotaoResult result = itemParamService.getItemParamByCid(itemCatId);
+        return result;
+    }
+
+    @PostMapping("/save/{cid}")
+    @ResponseBody
+    public TaotaoResult insertItemParam(@PathVariable long cid, String paramData){
+        TbItemParam tbItemParam = new TbItemParam();
+        tbItemParam.setItemCatId(cid);
+        tbItemParam.setParamData(paramData);
+
+        TaotaoResult result = itemParamService.insertItemPatam(tbItemParam);
+        return result;
+    }
 }
