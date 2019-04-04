610eff8e0ef22965dbc251761d87855f1f8f459e
LBJ <1172907815@qq.com>
2019-04-04 23:24:52 +0800

    新增jedisDAO层（编写完jedisSingle方法）

--- taotao-rest/src/main/java/guo/ping/taotao/rest/service/impl/ContentServiceImpl.java
+++ taotao-rest/src/main/java/guo/ping/taotao/rest/service/impl/ContentServiceImpl.java
@@ -5,6 +5,7 @@ import guo.ping.taotao.pojo.TbContent;
 import guo.ping.taotao.rest.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisCluster;
 
 import java.util.List;
 
@@ -14,8 +15,14 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper tbContentMapper;
 
+    /**
+     * 获取内容列表服务
+     * @param cid
+     * @return
+     */
     @Override
     public List<TbContent> getContentList(Long cid) {
+        //到redis缓存中查询是否有
         return tbContentMapper.getContentListByCategoryId(cid);
     }
 }
